c727dbc87ec3dec706732d65583aa2bc855aae2f
malweis <58945171+malweis@users.noreply.github.com>
2023-05-23 09:54:29 -0400

    fix: Añade link e icono de whatsapp
    el icon no estaba importado, icono cambia a
    icono de share y funcion añadida
    para parsear el mensaje

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -27,6 +27,12 @@ const Card = ({ record }) => {
         return 'default.png';
     }
   };
+  
+  const createWhatsAppLink = () => {
+    const message = `Hola, quiero contactarme contigo acerca de la donación de sangre. Mi nombre es ${record.nombre_apellido_donatario}, mi teléfono es ${record.creado_por}, mi CI es ${record.cedula_donatario}, y necesito ${record.volumenes_necesarios} volumen(es) de sangre ${record.tipo_sangre}. ¿Podemos coordinar la donación en ${record.establecimiento} antes de la fecha límite ${record.fecha_limite}?`;
+    const encodedMessage = encodeURIComponent(message);
+    return `https://wa.me/595986501547?text=${encodedMessage}`;
+  };
 
   const bloodTypeImage = getBloodTypeImage(record.tipo_sangre);
   
@@ -43,8 +49,8 @@ const Card = ({ record }) => {
           </svg>
         </div>
         <div id="whatsapp">
-          <a href="https://wa.me/595986501547?text=esto es un test a">
-            <ion-icon name="logo-whatsapp" className=" text-2xl"></ion-icon>
+          <a href={createWhatsAppLink()}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-share-2"><circle cx="18" cy="5" r="3"></circle><circle cx="6" cy="12" r="3"></circle><circle cx="18" cy="19" r="3"></circle><line x1="8.59" y1="13.51" x2="15.42" y2="17.49"></line><line x1="15.41" y1="6.51" x2="8.59" y2="10.49"></line></svg>
           </a>
         </div>
       </div>
